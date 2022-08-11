362e434d4bad990be07d1933a85ab23106d38e6a
Zhang honghong <zhanghonghong0505@163.com>
2022-08-11 11:04:31 +0800

    daily

---
+++ Stack0810/Stack0810/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack0810
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            Console.WriteLine("===============PUSH===============");
+            
+            stack.Push(0);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine("TOP OF THE STACK:" + stack.Peek());
+
+            Console.WriteLine("===============POP===============");
+
+            stack.Pop();
+
+            Console.WriteLine("TOP OF THE STACK " + stack.Peek());
+
+            Console.WriteLine("===============CLEAR===============");
+
+            stack.Clear();
+        }
+    }
+}
+
+
---
+++ Stack0810/Stack0810/Stack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Stack<T>
+{
+    private List<T> list;
+
+    public Stack()
+    {
+        list = new List<T>();
+    }
+
+    public void Clear()
+    {
+        list.Clear();
+    }
+
+    public void Push(T item)
+    {
+        list.Add(item);
+    }
+
+
+    public void Pop()
+    {
+        if (IsEmpty())
+        {
+            throw new IndexOutOfRangeException();
+        }
+        else
+        {
+            list.RemoveAt(Length() - 1);
+        }
+    }
+
+    public T Peek()
+    {
+        if (IsEmpty())
+        {
+            throw new IndexOutOfRangeException();
+        }
+        else
+        {
+            return list[Length() - 1];
+        }
+    }
+
+    public bool IsEmpty()
+    {
+        return (Length() == 0);
+    }
+
+    public int Length()
+    {
+        return list.Count();
+    }
+
+}
+
